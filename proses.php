7c335ca4e7b8cf73a911a6eefaec015c49ed2adb
Ahdadin <bagusahdadin@gmail.com>
2025-04-28 12:50:03 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Nama kamu:";
+    echo $_GET['nama'];
+    echo "<br />";
+    echo "Wakil Ketua";
+    echo $_GET['Wakil_ketua'];
+    echo "<br />";
+echo "sekretaris:";
+    echo $_GET{'sekretaris'};
+    ecgo "<br />";
+    echo "Bendahara:";
+    echo $_GET['Bendahara'];
+    echo "<br />";
+?>
